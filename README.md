25e21d597c829e8700979c869eb9bbc47d9539ac
Steffen Busch <steffen.busch@gmail.com>
2024-10-26 01:22:21 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# caddy-extra-placeholders
-Extra Placeholders plugin for Caddy
+# Extra Placeholders Caddy Plugin
+
+This repository contains the **Extra Placeholders** plugin for the [Caddy](https://caddyserver.com) server. This plugin provides additional placeholders that can be used for enhanced server-side information, such as system load averages, random numbers, Caddy version details, and system uptime.
+
+## Features
+
+This plugin introduces new placeholders that can be used within Caddy configurations:
+
+| Placeholder                    | Description                                       |
+|--------------------------------|---------------------------------------------------|
+| `{extra.caddy.version.simple}` | Simple version information of the Caddy server.   |
+| `{extra.caddy.version.full}`   | Full version information of the Caddy server.     |
+| `{extra.rand.float}`           | Random float value between 0.0 and 1.0.           |
+| `{extra.rand.int.0-100}`       | Random integer value between 0 and 100.           |
+| `{extra.load1}`                | System load average over the last 1 minute.       |
+| `{extra.load5}`                | System load average over the last 5 minutes.      |
+| `{extra.load15}`               | System load average over the last 15 minutes.     |
+| `{extra.uptime}`               | System uptime in a human-readable format.         |
+
+These placeholders can be used in Caddyfiles to provide dynamic content and system information in responses.
+
+## Building
+
+To build Caddy with this module, use [xcaddy](https://github.com/caddyserver/xcaddy):
+
+```bash
+$ xcaddy build --with github.com/steffenbusch/caddy-extra-placeholders
+```
+
+## Caddyfile config
+
+By default, the `extra_placeholders` directive is ordered before `redir` in the Caddyfile. This simplifies configuration and removes the need for manual ordering in most cases.
+However, if this default order does not suit your needs, you can still change the order using the [`order`](https://caddyserver.com/docs/caddyfile/directives#directive-order) global directive.
+
+To use the extra placeholders, you can add the following directive to your Caddyfile:
+
+```caddyfile
+:8080 {
+    extra_placeholders
+
+    respond "Caddy Version: {extra.caddy.version.full}, Uptime: {extra.uptime}"
+}
+```
+
+This example demonstrates how to use the additional placeholders provided by this plugin to dynamically insert Caddy version information and system uptime into an HTTP response.
+
+### Example: Conditional Redirect Based on Random Value
+
+The following example demonstrates how you can use conditional expressions with the random integer placeholder to redirect users to different search engines based on the generated random number:
+
+```caddyfile
+:8080 {
+    extra_placeholders
+
+    @redirectToGoogle expression `{extra.rand.int.0-100} <= 25`
+    redir @redirectToGoogle https://www.google.com
+
+    @redirectToBing expression `{extra.rand.int.0-100} > 25 && {extra.rand.int.0-100} <= 50`
+    redir @redirectToBing https://www.bing.com
+
+    @redirectToYahoo expression `{extra.rand.int.0-100} > 50 && {extra.rand.int.0-100} <= 75`
+    redir @redirectToYahoo https://www.yahoo.com
+
+    @redirectToDuckDuckGo expression `{extra.rand.int.0-100} > 75`
+    redir @redirectToDuckDuckGo https://www.duckduckgo.com
+}
+```
+
+In this example:
+
+- If `{extra.rand.int.0-100}` is between 0 and 25, the request is redirected to **Google** ([https://www.google.com](https://www.google.com)).
+- If `{extra.rand.int.0-100}` is between 26 and 50, the request is redirected to **Bing** ([https://www.bing.com](https://www.bing.com)).
+- If `{extra.rand.int.0-100}` is between 51 and 75, the request is redirected to **Yahoo** ([https://www.yahoo.com](https://www.yahoo.com)).
+- If `{extra.rand.int.0-100}` is greater than 75, the request is redirected to **DuckDuckGo** ([https://www.duckduckgo.com](https://www.duckduckgo.com)).
+
+This example demonstrates how to use the random integer placeholder in combination with conditional expressions to create dynamic redirection rules.
+
+## License
+
+This project is licensed under the Apache License 2.0. See the [LICENSE](LICENSE) file for details.
+
+## Acknowledgements
+
+- [Caddy](https://caddyserver.com) for providing a powerful and extensible web server.
+- [gopsutil](https://github.com/shirou/gopsutil) for system metrics, such as load averages and uptime, used under the BSD 3-Clause License.
---
+++ extra_placeholders.go
@@ -0,0 +1,120 @@
+// Copyright 2024 Steffen Busch
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// 	http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package extraplaceholders
+
+import (
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/shirou/gopsutil/v4/host"
+	"github.com/shirou/gopsutil/v4/load"
+)
+
+// ExtraPlaceholders represents the structure for the plugin.
+type ExtraPlaceholders struct{}
+
+func init() {
+	// Register the module with Caddy and specify where in the directive order it should be applied.
+	caddy.RegisterModule(ExtraPlaceholders{})
+	httpcaddyfile.RegisterHandlerDirective("extra_placeholders", parseCaddyfile)
+	httpcaddyfile.RegisterDirectiveOrder("extra_placeholders", "before", "redir")
+}
+
+// Placeholder | Description
+// ------------|-------------
+// `{extra.caddy.version.simple}` | The simple version of the Caddy server.
+
+// `{extra.caddy.version.simple}` | Simple version information of the Caddy server.
+// `{extra.caddy.version.full}` | Full version information of the Caddy server.
+// `{extra.rand.float}` | Random float value between 0.0 and 1.0.
+// `{extra.rand.int.0-100}` | Random integer value between 0 and 100.
+// `{extra.load1}` | System load average over the last 1 minute.
+// `{extra.load5}` | System load average over the last 5 minutes.
+// `{extra.load15}` | System load average over the last 15 minutes.
+// `{extra.uptime}` | System uptime in a human-readable format.
+
+// CaddyModule returns the module information required by Caddy to register the plugin.
+func (ExtraPlaceholders) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.extra_placeholders",
+		New: func() caddy.Module { return new(ExtraPlaceholders) },
+	}
+}
+
+// ServeHTTP adds new placeholders and passes the request to the next handler in the chain.
+func (ExtraPlaceholders) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	// Retrieve the replacer from the request context.
+	repl, ok := r.Context().Value(caddy.ReplacerCtxKey).(*caddy.Replacer)
+	if !ok {
+		return caddyhttp.Error(http.StatusInternalServerError, nil)
+	}
+
+	// Set Caddy version placeholders.
+	simpleVersion, fullVersion := caddy.Version()
+	repl.Set("extra.caddy.version.simple", simpleVersion)
+	repl.Set("extra.caddy.version.full", fullVersion)
+
+	// Set placeholders for random float and integer values.
+	repl.Set("extra.rand.float", rand.Float64())
+	repl.Set("extra.rand.int.0-100", rand.Intn(101))
+
+	// Set placeholders for system load averages (1, 5, and 15 minutes).
+	loadAvg, err := load.Avg()
+	if err == nil {
+		repl.Set("extra.load1", loadAvg.Load1)
+		repl.Set("extra.load5", loadAvg.Load5)
+		repl.Set("extra.load15", loadAvg.Load15)
+	}
+
+	// Set placeholder for system uptime.
+	uptime, err := host.Uptime()
+	if err == nil {
+		uptimeDuration := time.Duration(uptime) * time.Second
+		repl.Set("extra.uptime", uptimeDuration.String())
+	} else {
+		repl.Set("extra.uptime", "error retrieving uptime")
+	}
+
+	// Call the next handler in the chain.
+	return next.ServeHTTP(w, r)
+}
+
+// parseCaddyfile parses tokens from the Caddyfile into a new ExtraPlaceholders instance.
+func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var m ExtraPlaceholders
+	err := m.UnmarshalCaddyfile(h.Dispenser)
+	return m, err
+}
+
+// UnmarshalCaddyfile processes the configuration from the Caddyfile.
+func (e *ExtraPlaceholders) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	// Consume the directive name and ensure no extra arguments are provided.
+	d.Next()
+	if d.NextArg() {
+		return d.ArgErr()
+	}
+	return nil
+}
+
+// Interface guards to ensure ExtraPlaceholders implements the necessary interfaces.
+var (
+	_ caddy.Module                = (*ExtraPlaceholders)(nil)
+	_ caddyhttp.MiddlewareHandler = (*ExtraPlaceholders)(nil)
+)
